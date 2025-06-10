0b39a023b18e39e417b6bfc737406d3580b3a6b1
priyakumariyt05 <priya.kumari@quadbtech.com>
2025-06-10 08:30:15 +0530

    update herosection,add ecosystemMob,change font size in ai,game,blockchain section, and add user select none in global css

--- src/app/AIServices/page.tsx
+++ src/app/AIServices/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 import React from 'react';
+import { useEffect, useState } from "react";
 import HeroSection from './HeroSection';
 import AIServicesSection from './AIServicesSection';
 import AiFaq from './AiFaq';
@@ -7,14 +8,29 @@ import EcosystemSection from '../Reusable/EcosystemSection';
 import Contact from "../Reusable/Contact"
 import WhyChooseAi from "./WhyChooseAi"
 import WhatWeOfferAi from "./WhatWeOfferAi"
+import EcosystemMobile from '../Reusable/EcoSystemMobile';
 const AIServicesPage: React.FC = () => {
+     const [isMobile, setIsMobile] = useState(false);
+  
+    useEffect(() => {
+      const handleResize = () => {
+        setIsMobile(window.innerWidth < 768);
+      };
+  
+      handleResize(); 
+      window.addEventListener("resize", handleResize); 
+  
+      return () => {
+        window.removeEventListener("resize", handleResize); 
+      };
+    }, []);
   return (
     <div>
    <HeroSection />
    <AIServicesSection/>
 <WhatWeOfferAi/>
       <WhyChooseAi />
-      <EcosystemSection />
+  {isMobile ?<EcosystemMobile/> : <EcosystemSection/>}
       <AiFaq />
       <Contact/>
 
--- src/app/GameDevelopment/page.tsx
+++ src/app/GameDevelopment/page.tsx
@@ -1,5 +1,6 @@
-
+'use client'
 import React from 'react';
+import { useEffect, useState } from "react";
 import HeroSection from './HeroSection';
 import BlockchainSection from './GameDevlopmentSection';
 import WhyChooseGame from "./WhyChooseGame"
@@ -8,15 +9,30 @@ import WhatWeOfferGame from "./WhatWeOfferGame"
 import FaqGame from './FaqGame';
 import Contact from "../Reusable/Contact"
 import EcosystemSection from '../Reusable/EcosystemSection';
+import EcosystemMobile from '../Reusable/EcoSystemMobile';
 
 const GameDevelopmentPage: React.FC = () => {
+      const [isMobile, setIsMobile] = useState(false);
+  
+    useEffect(() => {
+      const handleResize = () => {
+        setIsMobile(window.innerWidth < 768);
+      };
+  
+      handleResize(); 
+      window.addEventListener("resize", handleResize); 
+  
+      return () => {
+        window.removeEventListener("resize", handleResize); 
+      };
+    }, []);
   return (
     <div>
    <HeroSection />
    <BlockchainSection/>
       <WhatWeOfferGame />
       <WhyChooseGame />
-      <EcosystemSection/>
+    {isMobile ?<EcosystemMobile/> : <EcosystemSection/>}
       <FaqGame />
       <Contact/>
 
