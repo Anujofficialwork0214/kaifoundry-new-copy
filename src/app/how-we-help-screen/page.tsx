6489f0eb01cea97c6fedd4feefba18c2b4a960c1
priyakumariyt05 <priya.kumari@quadbtech.com>
2025-06-17 19:56:02 +0530

    fix contact section issue

--- src/app/how-we-help-screen/page.tsx
+++ src/app/how-we-help-screen/page.tsx
@@ -8,20 +8,7 @@ import FileCoinMob from './FileCoinMob';
 
 
 const HowWeHelpSection: React.FC = () => {
-  const [isMobile, setIsMobile] = React.useState(false);
 
-  React.useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth < 768);
-    };
-
-    handleResize();
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
 
   return (
     <div>
